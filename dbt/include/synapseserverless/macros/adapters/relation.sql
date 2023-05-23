a0a42e7ea81ee903d959b049f374d50d2645e300
Huỳnh Phúc Đại <huynhphucdai@gmail.com>
2023-05-23 21:12:16 +0930

    Update relation.sql
    [Add] rename_relation implementation for Synapse Serverless. It only can rename view since table renaming is not supported and no better way to rename external tables

--- dbt/include/synapseserverless/macros/adapters/relation.sql
+++ dbt/include/synapseserverless/macros/adapters/relation.sql
@@ -1,3 +1,17 @@
+# This doesn't work with table and Synapse Serverless doesn't support tabl as well
+# Implementation take from the conversation at: 
+# https://github.com/dbt-msft/dbt-synapse-serverless/issues/4
+{% macro synapseserverless__rename_relation(from_relation, to_relation) -%}
+  {% call statement('rename_relation') -%}
+    DECLARE @to_definition nvarchar(max);
+    SET @to_definition = replace(object_definition (object_id ('{{ from_relation.include(database=False) }}')), '{{ from_relation.include(database=False) }}', '{{ to_relation.include(database=False) }}');
+    EXEC('DROP VIEW {{ from_relation.include(database=False) }}')
+    EXEC('DROP VIEW IF EXISTS {{ to_relation.include(database=False) }}')
+    EXEC(@to_definition)
+  {%- endcall %}
+{% endmacro %}
+
+
 {% macro synapseserverless__drop_relation(relation) -%}
     {% call statement('drop_relation', auto_begin=False) -%}
       {{ synapseserverless__drop_relation_script(relation) }}
